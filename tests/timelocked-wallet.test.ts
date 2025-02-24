0d2c0c93c9c43c63dd7a78902d54a8c81efff08b
Nnamdi Okeke <onnamdialex@gmail.com>
2025-02-24 16:01:57 +0100

    :white_check_mark: Add unit test for the public functions | wip ...

--- tests/timelocked-wallet.test.ts
+++ tests/timelocked-wallet.test.ts
@@ -1,21 +1,126 @@
-
 import { describe, expect, it } from "vitest";
 
-const accounts = simnet.getAccounts();
-const address1 = accounts.get("wallet_1")!;
+Clarinet.test({
+  name: "Allows the contract owner to lock an amount",
+  async fn(chain: Chain, accounts: Map<string, account>) {
+    const deployer = accounts.get("deployer")!;
+    const beneficiary = accounts.get("wallet_1")!;
+    const amount = 10;
+    const block = chain.mineBlock([
+      Tx.contractCall(
+        "timelocked-wallet",
+        "lock",
+        [
+          types.principal(beneficiary.address),
+          types.uint(10),
+          types.uint(amount),
+        ],
+        deployer.address
+      ),
+    ]);
+    // The lock should be successful.
+    block.receipts[0].result.expectOk().expectBool(true);
+    // There should be a STX transfer of the amount specified.
+    block.receipts[0].events.expectSTXTransferEvent(
+      amount,
+      deployer.address,
+      `${deployer.address}.timelocked-wallet`
+    );
+  },
+});
 
-/*
-  The test below is an example. To learn more, read the testing documentation here:
-  https://docs.hiro.so/stacks/clarinet-js-sdk
-*/
+Clarinet.test({
+  name: "Does not allow anyone else to lock an amount",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const accountA = accounts.get("wallet_1")!;
+    const beneficiary = accounts.get("wallet_2")!;
+    const block = chain.mineBlock([
+      Tx.contractCall(
+        "timelocked-wallet",
+        "lock",
+        [types.principal(beneficiary.address), types.uint(10), types.uint(10)],
+        accountA.address
+      ),
+    ]);
 
-describe("example tests", () => {
-  it("ensures simnet is well initalised", () => {
-    expect(simnet.blockHeight).toBeDefined();
-  });
+    // Should return err-owner-only (err u100).
+    block.receipts[0].result.expectErr().expectUint(100);
+  },
+});
 
-  // it("shows an example", () => {
-  //   const { result } = simnet.callReadOnlyFn("counter", "get-counter", [], address1);
-  //   expect(result).toBeUint(0);
-  // });
+Clarinet.test({
+  name: "Cannot lock more than once",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const beneficiary = accounts.get("wallet_1")!;
+    const amount = 10;
+    const block = chain.mineBlock([
+      Tx.contractCall(
+        "timelocked-wallet",
+        "lock",
+        [
+          types.principal(beneficiary.address),
+          types.uint(10),
+          types.uint(amount),
+        ],
+        deployer.address
+      ),
+      Tx.contractCall(
+        "timelocked-wallet",
+        "lock",
+        [
+          types.principal(beneficiary.address),
+          types.uint(10),
+          types.uint(amount),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    // The first lock worked and STX were transferred.
+    block.receipts[0].result.expectOk().expectBool(true);
+    block.receipts[0].events.expectSTXTransferEvent(
+      amount,
+      deployer.address,
+      `${deployer.address}.timelocked-wallet`
+    );
+
+    // The second lock fails with err-already-locked (err u101).
+    block.receipts[1].result.expectErr().expectUint(101);
+
+    // Assert there are no transfer events.
+    assertEquals(block.receipts[1].events.length, 0);
+  },
+});
+
+Clarinet.test({
+  name: "Unlock height cannot be in the past",
+  async fn(chain: Chain, accounts: Map<string, Account>) {
+    const deployer = accounts.get("deployer")!;
+    const beneficiary = accounts.get("wallet_1")!;
+    const targetBlockHeight = 10;
+    const amount = 10;
+
+    // Advance the chain until the unlock height plus one.
+    chain.mineEmptyBlockUntil(targetBlockHeight + 1);
+
+    const block = chain.mineBlock([
+      Tx.contractCall(
+        "timelocked-wallet",
+        "lock",
+        [
+          types.principal(beneficiary.address),
+          types.uint(targetBlockHeight),
+          types.uint(amount),
+        ],
+        deployer.address
+      ),
+    ]);
+
+    // The second lock fails with err-unlock-in-past (err u102).
+    block.receipts[0].result.expectErr().expectUint(102);
+
+    // Assert there are no transfer events.
+    assertEquals(block.receipts[0].events.length, 0);
+  },
 });
